4b898ce5da4439796358e752505b980c745cde1b
Henry H <iisdndr@gmail.com>
2018-06-19 21:41:25 -0700

    Added initial exercise subset

---
+++ 00 - Basics and Printing/03/readme.md
@@ -0,0 +1,1 @@
+Compilation practice w/ `javac`
---
+++ 00 - Basics and Printing/07/MyInitials.java
@@ -0,0 +1,12 @@
+public class MyInitials {
+	public static void main(String[] args) {
+		System.out.println("For my name, my initials are... \n");
+		System.out.println("H   H   SSS   H    H");
+		System.out.println("H   H  S   S  H    H");
+		System.out.println("H   H  S      H    H");
+		System.out.println("HHHHH   SSS   HHHHHH");
+		System.out.println("H   H      S  H    H");
+		System.out.println("H   H  S   S  H    H");
+		System.out.println("H   H   SSS   H    H");
+	}
+}
